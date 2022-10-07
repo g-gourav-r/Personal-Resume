6577ab25a4b851d7ebb32fdd0310aa6fffde88cb
Gourav <gouravrchandra@gmail.com>
2022-10-07 11:16:43 +0530

    Made major changes

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,7 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+  }
